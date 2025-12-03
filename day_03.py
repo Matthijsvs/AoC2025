cc3ad88b3482d05f2d1a535742fd6b8d20a77964
NLSTAMAT <matthijs.vanstappen@sew-eurodrive.nlo>
2025-12-03 13:00:51 +0100

    dag 3 werkend

--- day_03.py
+++ day_03.py
@@ -1,52 +1,26 @@
 from get_src import *
-import re
 
-sample = """987654321111111
-811111111111119
-234234234234278
-818181911112111
-"""
+def get_jolts(battery_list, max):
+    curr_num = ""                           #result
+    battery_list_remaining=battery_list     #battery list from last chosen number
+    while len(curr_num)<max:
+        todo=max-len(curr_num)              #number of digits we still need
 
+        biggest_number = list(battery_list_remaining[:len(battery_list_remaining)+1-todo])
+        biggest_number.sort(reverse=True)
+        next = battery_list_remaining.index(biggest_number[0])
+        curr_num+=battery_list_remaining[next]
 
-def get_jolts(s,max):
-    #we take this battery
-    opts=[]
-    maxnum=0
-    for i in range(9,0,-1):
-        print(f"start with {i}")
-        start = [m.start() for m in re.finditer(str(i), s)]
-        for j in start:
-            if j<=len(s)-max:
-                print(f"begin with the {i} on position {j}")
-
-    opts=list(opts)
-    opts.sort(reverse=True)
-    print(opts)
-
-    for i in opts:
-        if len(i)==max:
-            print(i,len(i))
-            return int(i)
+    return int(curr_num)
 
 
 
-inp = get(sample)
+inp = get()
 part_a=0
+part_b=0
 for i in inp.splitlines():
-    part_a+=get_jolts(i,12)
-    """max1=0
-    j = list(i)
-    j.sort(reverse=True)
-    while j:
-        largest=j.pop(0)
-        pos = i.index(largest)
-        for n in range(pos+1,len(i)):
-            t = int(i[pos]+i[n])
+    part_a += get_jolts(i, 2)
+    part_b += get_jolts(i,12)
 
-            if t>max1:
-                max1=t
-    part_a+=max1
-    """
-
-print()
 print(part_a)
+print(part_b)
