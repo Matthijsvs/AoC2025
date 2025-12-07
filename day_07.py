eabba806796e2b9fa26a9446fb7e2b24c54105b1
Matthijsvs <46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com>
2025-12-07 06:43:55 +0100

    dag 7 opgeschoond

--- day_07.py
+++ day_07.py
@@ -1,30 +1,23 @@
-import copy
 from get_src import *
-
 inp = get_grid()
 
-beams=[]
+beam_count = [0]*len(inp[0])
+
 for x in range(len(inp[0])):
     for y in range(len(inp)):
         if inp[y][x]=="S":
-            beams.append(Point(x,y))
-            start=Point(x,y)
+            beam_count[x] = 1
             break
 
-beam_count = [0]*len(inp[0])
-beam_count[start.x]=1
 split=0
 for y in range(len(inp)):
-    new_count = copy.deepcopy(beam_count)
     for x in range(len(inp[0])):
-
         if inp[y][x]=="^":
             if beam_count[x]>0:
                 split+=1
-                new_count[x-1] += new_count[x]
-                new_count[x+1] += new_count[x]
-                new_count[x] =0
-    beam_count=new_count
+                beam_count[x-1] += beam_count[x]
+                beam_count[x+1] += beam_count[x]
+                beam_count[x] =0
 
 print(f"part_a:{split}")
 print(f"part_b:{sum(beam_count)}")
