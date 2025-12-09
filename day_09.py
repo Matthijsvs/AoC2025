df582794e0fed5adf7b1345998fcbce5755155df
Matthijsvs <46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com46461044+Matthijsvs@users.noreply.github.com>
2025-12-09 07:41:50 +0100

    dag 9 - werkend

--- day_09.py
+++ day_09.py
@@ -1,31 +1,19 @@
 from get_src import *
-from shapely.geometry import Point,LineString
+from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-
-sample = """7,1
-11,1
-11,7
-9,7
-9,5
-2,5
-2,3
-7,3
-"""
 inp = get()
 coords = []
-lines=[]
 for i in inp.splitlines():
     x,y=i.split(",")
     coords.append(Point(int(x),int(y)))
 
-#coords.append(coords[0])
 polygon = Polygon(coords)
-#print(polygon.exterior)
-
+with open('day_09.svg', 'w') as f:
+    f.write(polygon._repr_svg_())
 
 def dist(a,b):
-    area = abs(coords[a].x-coords[b].x+1)*abs(coords[a].y-coords[b].y+1)
+    area = (abs(coords[a].x-coords[b].x)+1)*(abs(coords[a].y-coords[b].y)+1)
     return int(area)
 
 dist_list=[]
@@ -36,18 +24,16 @@ for i in range(len(coords)):
         dist_list.append((distance, i,j))
         if distance < min_dist:
             min_dist = distance
-dist_list.sort(reverse=True)  # sort tuples by lowest distance
+dist_list.sort(reverse=True)  # sort tuples by biggest area
 
-print(dist_list[0])
-#def pass_line(pt):
+print(dist_list[0][0])
 
-Found = False
-while not Found:
+while True:
     area,start,end = dist_list.pop(0)
     start = coords[start]
     end = coords[end]
     t = Polygon([(start.x,start.y),(end.x,start.y),(end.x,end.y),(start.x,end.y)])
-    #print(t.within(polygon),t)
-    if t.within(polygon):
-        Found=True
-print(area)
+
+    if polygon.covers(t):
+        print(area)
+        break
