c21e6a4651cb15b362d80da36ed07b8120299092
NLSTAMAT <matthijs.vanstappen@sew-eurodrive.nlo>
2025-12-12 12:17:13 +0100

    dag 10 - opgeschoond

--- day_10.py
+++ day_10.py
@@ -1,20 +1,15 @@
 from get_src import *
-from sympy import symbols, Eq, solve,linsolve,solveset
+from pulp import *
 
-sample = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-[#####.#] (2,4,5,6) (0,1,4,5) (0,1,2,3,4,5) (1,3,5,6) (0,1,2,4,6) (1,3,6) (0,1,5,6) {49,71,32,29,52,70,63}
-"""
 def bitfield(n,d):
     s = bin(n)[2:].zfill(d)
     return [int(digit) for digit in s] # [2:] to chop off the "0b" part
 
 def clean(x):
-    x = x.strip("(){}")
+    x = x.strip("(){}")     #returns a python list of a string between brackets
     return list(map(int,x.split(",")))
 
-inp = get(sample)
+inp = get()
 part_a=0
 part_b=0
 for i in inp.splitlines():
@@ -29,38 +24,28 @@ for i in inp.splitlines():
     for possib in range(m):
         state = [False for x in indicators]
         b = bitfield(possib, len(buttons))
-        #print(b,end="")
         for switch in range(len(b)):
-            if b[switch]==1:
-                #switch is touched
+            if b[switch]==1:     #switch is touched
                 for j in buttons[switch]:
-                    #print(f"toggling {j}")
-                    state[j]= not state[j]
-        #print(state)
+                    state[j]= not state[j]  #toggle state
         if state == indicators:
-            #print("-->",b)
-            ans.append(sum(b))
+            ans.append(sum(b))  #array of 0/1 so sum is equal to number of 1 bits
     part_a+=min(ans)
     #--------------------------------- part b
-    print(jolts,buttons)
-    joltage = 0
-    for j in jolts[::-1]:
-        joltage = joltage * 1000 + j
-    print(joltage)
-
-    bs = []
-    for x in buttons:
-        y = 0
-        for z in x:
-            y+=1000**z
-        bs.append(y)
-
-    bs.extend([0]*10)
-
-    x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 = symbols('x0:10')
-
-    equation = [x1*bs[0]+x2*bs[1]+x3*bs[2]+x4*bs[3]+x5*bs[4]+x6*bs[5]+x7*bs[6]- joltage]
-    print(linsolve(equation,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10))
-    print("-----------")
-
+    prob = LpProblem("The_Button_Problem", LpMinimize)
+    butt = [f"B{x}" for x in range(len(buttons))]
+    button_vars = LpVariable.dicts("Press", butt, 0, 300, LpInteger)
+    prob += (
+        lpSum([button_vars[i] for i in butt]), "total presses")
+    t = []
+    for j in range(len(jolts)):
+        ex = []
+        for b in buttons:
+            if j in b:
+                ex.append(f"B{buttons.index(b)}")
+        prob += (lpSum([1 * button_vars[i] for i in ex]) == jolts[j], f"requiredJolts{j}")
+    prob.solve()
+    part_b+=int(value(prob.objective))
+    print("-------------------")
 print(part_a)
+print(part_b)
